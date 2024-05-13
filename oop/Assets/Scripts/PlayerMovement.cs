ca6ded64c7e9be9302ea2ada96c0c5738310d97b
meryrubio <mariarg9d@gmail.com>
2024-05-13 12:52:42 +0200

    cdfbhdrt

--- oop/Assets/Scripts/PlayerMovement.cs
+++ oop/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private Vector2 _dir;
     public KeyCode leftKey = KeyCode.A, rightKey = KeyCode.D;
+
+    Characters.Character charcater;
+
+    SpriteRenderer rend;
+    Animator animator;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        //programamos un if para elegir para la funcion del boton para elegir el personaje
+        if (GameManager.instance.characterType == Characters.WIZARD)
+        {
+            character = new Characters.Wizard(speed, GetComponent<Rigidbody2D>());
+        }
+        else if (GameManager.instance.characterType == Characters.COWBOY)
+        {
+            character = new Characters.Cowboy(speed, GetComponent<Rigidbody2D>());
+        }
+       
+
+        rend.sprite = character.GetSprite();
+        
+
+    }
     void Movement()
     {
 
