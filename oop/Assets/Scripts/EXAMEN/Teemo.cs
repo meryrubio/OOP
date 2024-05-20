6ac9884d4fc62d8b995355b2b0125bdae3993cc6
Hectormenta14 <webhector14@gmail.com>
2024-05-20 02:00:02 +0200

    para el examen

---
+++ oop/Assets/Scripts/EXAMEN/Teemo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teemo : MonoBehaviour
+{
+    public Teemo() : base("Teemo", 1000, Resources.Load<Sprite>("Sprites/beemo"))
+    {
+
+    }
+    public override float Attack()  // Metodo Attack que recibe de Character
+    {
+        return damage;
+    }
+    public override float Heal()  // Metodo Heal que recibe de Character
+    {
+        health += 1000f * damage;
+        base.Heal();
+        return health;
+    }
+}
